684932a8fd6266826e8fe73cf436ec56ca56c38d
chrisdadev13 <chrisdadev13@gmail.com>
2022-11-07 13:36:01 -0400

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: "eslint:recommended",
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "2021",
+    sourceType: "module",
+  },
+  rules: {},
+};
