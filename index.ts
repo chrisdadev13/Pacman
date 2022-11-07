e7f4041e9b2655402037db3db280e4feae0e5b00
chrisdadev13 <chrisdadev13@gmail.com>
2022-11-07 14:18:02 -0400

    creating the map items

--- index.ts
+++ index.ts
@@ -1,3 +1,27 @@
-const canvas = document.querySelector("canvas");
+const canvas = document.querySelector("canvas") as HTMLCanvasElement;
 
-const c = canvas?.getContext("2d");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+type Position = {
+  x: number;
+  y: number;
+};
+
+class Boundary {
+  position: Position;
+  width: number;
+  height: number;
+
+  constructor(position: Position) {
+    this.position = position;
+    this.width = 40;
+    this.height = 40;
+  }
+
+  draw() {
+    c?.fillRect(this.position.x, this.position.y, this.width, this.height);
+  }
+}
